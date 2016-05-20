a62f8ea10ff63f38ac1f1ec8204c84fd247b4610
jeff <jeff@darkware.org>
2016-05-20 00:16:31 -0400

    Added support for retrieving taxonomies and terms.

--- src/main/java/org/darkware/wpman/data/WPBlog.java
+++ src/main/java/org/darkware/wpman/data/WPBlog.java
@@ -23,15 +23,27 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.base.Objects;
 import com.google.common.reflect.TypeToken;
 import org.darkware.lazylib.LazyLoaded;
+import org.darkware.lazylib.LazyLoadedMap;
 import org.darkware.wpman.WPManager;
 import org.darkware.wpman.util.serialization.MinimalUpdatableSerializer;
 import org.darkware.wpman.wpcli.WPCLI;
 import org.darkware.wpman.wpcli.WPCLIFieldsOption;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
+ * A {@code WPBlog} represents a single blog ("site" in the legacy terminology) within a WordPress installation.
+ * In single-site installs, this should be roughly equivalent to the entire install, and most queries will return
+ * the same set of data for the blog and the instance as a whole. In multisite installs, there may be significant
+ * differences between a blog and the encompassing network (sometimes confusingly termed a "site"). In multisite
+ * instances, a blog has a set of users which is a strict subset of the network user list, an identical set of
+ * plugins with different activation states, and a set of themes which matches just those themes activated on
+ * the network level.
+ *
  * @author jeff
  * @since 2016-01-23
  */
@@ -59,23 +71,43 @@ public class WPBlog extends WPComponent
         return blogCommand;
     }
 
-    @JsonProperty("blog_id") private int blogId;
-    @JsonProperty("domain") private String domain;
-    @JsonProperty("url") private String url;
-    @JsonProperty("last_updated") private LocalDateTime lastModified;
-    @JsonProperty("registered") private LocalDateTime creationDate;
+    @JsonProperty("blog_id")
+    private int blogId;
+    @JsonProperty("domain")
+    private String domain;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("last_updated")
+    private LocalDateTime lastModified;
+    @JsonProperty("registered")
+    private LocalDateTime creationDate;
 
-    @JsonProperty("public") private boolean searchable;
-    @JsonProperty("deleted") private boolean deleted;
+    @JsonProperty("public")
+    private boolean searchable;
+    @JsonProperty("deleted")
+    private boolean deleted;
 
     @JsonProperty("users")
     private final WPBlogUsers users;
 
-    private String subDomain;
+    private transient String subDomain;
+
+    @JsonIgnore
     private final WPBlogPlugins plugins;
+    @JsonIgnore
     private final WPCron cron;
+
+    @JsonProperty("theme")
+    @JsonSerialize(using = MinimalUpdatableSerializer.class)
     private LazyLoaded<WPTheme> theme;
 
+    @JsonIgnore
+    private LazyLoadedMap<String, WPTaxonomy> taxonomies;
+
+    /**
+     * Create a new blog instance. The initial state is incomplete and requires setting at least the
+     * domain in order to fully support normal functionality.
+     */
     public WPBlog()
     {
         super();
@@ -102,116 +134,276 @@ public class WPBlog extends WPComponent
                 return activeThemes.get(0);
             }
         };
+
+        this.taxonomies = new LazyLoadedMap<String, WPTaxonomy>()
+        {
+            @Override
+            protected Map<String, WPTaxonomy> loadValues() throws Exception
+            {
+                WPCLI taxCmd = WPBlog.this.buildCommand("taxonomy", "list");
+                taxCmd.setBlog(WPBlog.this);
+                taxCmd.loadThemes(false);
+                taxCmd.loadPlugins(false);
+                WPTaxonomy.setFields(taxCmd);
+
+                Map<String, WPTaxonomy> taxMap = new HashMap<>();
+                for (WPTaxonomy tax : taxCmd.readJSON(new TypeToken<Set<WPTaxonomy>>(){}))
+                {
+                    tax.setBlog(WPBlog.this);
+                    taxMap.put(tax.getName(), tax);
+                }
+
+                return taxMap;
+            }
+        };
     }
 
+    /**
+     * Fetch the numeric ID assigned to this blog. This is mostly used for various internal references and
+     * some linking (such as the media library). In general, it is not publicly advertised.
+     *
+     * @return The ID of this blog as a positive integer.
+     */
     public int getBlogId()
     {
         return this.blogId;
     }
 
-    public void setBlogId(final int blogId)
+    /**
+     * Set the ID for this blog. The ID is required to be unique within the installation or network.
+     *
+     * @param blogId A positive integer.
+     * @throws IllegalArgumentException If the blog ID is zero or negative.
+     */
+    protected void setBlogId(final int blogId)
     {
+        if (blogId < 1) throw new IllegalArgumentException("Blog ID cannot be zero or negative.");
         this.blogId = blogId;
     }
 
+    /**
+     * Fetch the full domain assigned to this blog. In multisite installations this is a critical identifying
+     * characteristic of the blog. It acts as the unique identifier of the blog within the multisite network,
+     * and will be used by various other methods and objects for retrieving blog-specific data sets.
+     * <p>
+     * <em>Note:</em> This is the internal, canonical domain name assigned to the blog. Alternative domain names
+     * assigned by rewrite rules or plugins such as Domain Mapping are not reflected here and must not be used.
+     *
+     * @return The blog's official, internal domain name.
+     */
     public String getDomain()
     {
         return this.domain;
     }
 
-    public void setDomain(final String domain)
+    /**
+     * Sets the official domain for this blog. This is required to be unique within the multisite network. It
+     * is further required to be a strict subdomain of the network domain. For example, if the network's
+     * configured domain name is {@code examplewp.com}, the all blogs in the network <em>must</em> use
+     * domains which end in {@code examplewp.com}.
+     * <p>
+     * Updating this field during runtime <em>will not</em> modify any actual domain mapping within the
+     * installation. This is solely used for identifying the blog.
+     *
+     * @param domain The full, canonical domain name for the blog.
+     */
+    protected void setDomain(final String domain)
     {
         this.domain = domain;
     }
 
-    public String getSubDomain()
+    /**
+     * Fetch the official subdomain for this blog. This is defined to be the first hostname group of the
+     * official domain obtained by {@link #getDomain()}. This can be treated as a short hand for the domain
+     * since the remainder of the domain is defined to be the network domain and is shared by every blog
+     * on the network. Thus, the subdomain shares the same uniqueness as the domain.
+     *
+     * @return The subdomain for the blog.
+     */
+    public final String getSubDomain()
     {
         if (this.subDomain == null)
         {
             if (this.domain == null) return "<unknown>";
-            this.subDomain = domain.split("\\.", 2)[0];
+            this.subDomain = this.domain.split("\\.", 2)[0];
         }
         return this.subDomain;
     }
 
+    /**
+     * Fetch the URL for this blog. This is mostly useful as a convenience. It should match the blog's domain
+     * returned by {@link #getDomain()}, with the additional URI components to indicate an empty directory and
+     * the default protocol specification.
+     * <p>
+     * <em>Future Implementation:</em> This field would gain more value if support is added for directory-based
+     * multisite installations. In that case, the URL would provide a single-method way of fetching the
+     * official URL to contact the blog via, incorporating changes for both subdomains and subdirectories.
+     *
+     * @return The official URL as a {@code String}.
+     */
     public String getUrl()
     {
         return this.url;
     }
 
-    public void setUrl(final String url)
+    /**
+     * Set the URL for this blog. Since subdirectory-based multisite installs are not supported, this should
+     * always roughly match the domain name.
+     *
+     * @param url The official URL for this blog.
+     */
+    protected void setUrl(final String url)
     {
         this.url = url;
     }
 
+    /**
+     * Fetch the time that this blog was last modified. Modifications appear to include various actions including
+     * changing settings, activating plugins, creating posts, or uploading media.
+     *
+     * @return The last modification time, in UTC time.
+     */
     public LocalDateTime getLastModified()
     {
         return this.lastModified;
     }
 
-    public void setLastModified(final LocalDateTime lastModified)
+    /**
+     * Set the last time this blog was modified.
+     *
+     * @param lastModified The last modification time.
+     */
+    protected void setLastModified(final LocalDateTime lastModified)
     {
         this.lastModified = lastModified;
     }
 
+    /**
+     * Fetch the time and date that this blog was created.
+     *
+     * @return The creation date, in UTC time.
+     */
     public LocalDateTime getCreationDate()
     {
         return this.creationDate;
     }
 
-    public void setCreationDate(final LocalDateTime creationDate)
+    /**
+     * Set the creation date and time for this blog.
+     *
+     * @param creationDate A date and time, in UTC time.
+     */
+    protected void setCreationDate(final LocalDateTime creationDate)
     {
         this.creationDate = creationDate;
     }
 
+    /**
+     * Check if this blog is set to encourage Internet search engine indexing. This is usually realized as
+     * changes to the response for URLs requesting the {@code robots.txt} file. Searchable blogs will reply with
+     * permissive settings. Non-searchable blogs will reply with settings discouraging indexing. Ultimately, it
+     * is up to the search engine to honor these settings.
+     *
+     * @return {@code true} if the site encourages search engine indexing, {@code false} if it attempts to
+     * discourage search engines.
+     */
     public boolean isSearchable()
     {
         return this.searchable;
     }
 
-    public void setSearchable(final boolean searchable)
+    /**
+     * Set the flag declaring the blogs behavior toward search engines.
+     *
+     * @param searchable {@code true} if the site encourages search engine indexing, {@code false} if it
+     * attempts to discourage search engines.
+     * @see #isSearchable()
+     */
+    protected void setSearchable(final boolean searchable)
     {
         this.searchable = searchable;
     }
 
+    /**
+     * Check if this blog has been marked for deletion. Being marked for deletion does not trigger immediate
+     * deletion. Rather, it is treated more like a filesystem "dirty" flag or a desktop "trash" feature. The blog
+     * will continue to exist until some other process decides to actually delete it. Until that point, the flag
+     * can be unset and the blog would continue to exist as normal.
+     *
+     * @return {@code true} if the blog is marked to be deleted in the future, otherwise {@code false}.
+     */
     public boolean isDeleted()
     {
         WPManager.log.info("Blog:" + this.getBlogId() + " deleted=" + this.deleted);
         return this.deleted;
     }
 
+    /**
+     * Set the flag to mark this blog as waiting to be deleted. This does not actually cause deletion.
+     *
+     * @param deleted {@code true} if the blog should be marked as waiting for deletion, otherwise {@code false}.
+     */
     public void setDeleted(final boolean deleted)
     {
         this.deleted = deleted;
     }
 
     /**
-     * Fetch the users assigned to this blog.
+     * Fetch the users assigned to this blog. This includes only users who have been directly assigned to the
+     * blog and may not include users who have access to the site via superadmin privileges.
      *
      * @return A {@link WPBlogUsers} object attached to this blog.
      */
-    public WPBlogUsers getUsers()
+    public final WPBlogUsers getUsers()
     {
         return this.users;
     }
 
-    @JsonIgnore
-    public WPBlogPlugins getPlugins()
+    /**
+     * Fetch the collection of plugins recognized by this blog, along with the state of those plugins with
+     * respect to this blog.
+     *
+     * @return The collection of plugins as a {@link WPBlogPlugins} object.
+     */
+    public final WPBlogPlugins getPlugins()
     {
-        return plugins;
+        return this.plugins;
     }
 
-    @JsonProperty("theme")
-    @JsonSerialize(using = MinimalUpdatableSerializer.class)
+    /**
+     * Fetch the theme currently being used by this blog.
+     *
+     * @return The currently activated {@link WPTheme}.
+     */
     public WPTheme getTheme()
     {
         return this.theme.value();
     }
 
-    @JsonIgnore
+    /**
+     * Fetch cron subsystem data specific to this blog. This includes a list of registered events scheduled
+     * for execution within the blog.
+     *
+     * @return The cron data as a {@link WPCron} object associated with this blog.
+     */
     public WPCron getCron()
     {
         return this.cron;
+    }
+
+    /**
+     * Fetch the {@link WPTaxonomy} for the given name.
+     *
+     * @param taxonomyName The internal name for the taxonomy.
+     * @return A {@link WPTaxonomy}.
+     * @throws IllegalArgumentException If the taxonomy doesn't exist.
+     */
+    @JsonIgnore
+    public WPTaxonomy getTaxonomy(final String taxonomyName)
+    {
+        WPTaxonomy tax = this.taxonomies.map().get(taxonomyName);
+        if (tax == null) throw new IllegalArgumentException("The taxonomy '" + taxonomyName + "' does not exist.");
+        return tax;
     }
 
     @Override
@@ -220,12 +412,12 @@ public class WPBlog extends WPComponent
         if (this == o) return true;
         if (!(o instanceof WPBlog)) return false;
         final WPBlog wpBlog = (WPBlog) o;
-        return blogId == wpBlog.blogId;
+        return this.blogId == wpBlog.blogId;
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hashCode(blogId);
+        return Objects.hashCode(this.blogId);
     }
 }
